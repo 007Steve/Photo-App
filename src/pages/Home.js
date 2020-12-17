e8641be599dd5b189befe1dadebecc25c6943cb5
007steve <stephen.plummer1.sp@gmail.com>
2020-12-17 01:12:01 -0500

    added post to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import "../styles/Home.css"
+import React from "react";
+import "../styles/Home.css";
+import Nav from "../components/Nav";
+import Row from "../components/Row";
+import Post from "../components/Post";
 function Home() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="home">
+      <Nav />
+      <Row />
+      <div className="home__container">
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+
+        <Post />
+        <Post />
+        <Post />
+
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
