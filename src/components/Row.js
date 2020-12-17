82801fddb32722f36f459c9433d6f9b2de03b7c4
007steve <stephen.plummer1.sp@gmail.com>
2020-12-17 01:13:46 -0500

    added row container

---
+++ src/components/Row.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../styles/Row.css"
+import RowCard from './RowCard'
+function Row() {
+    return (
+        <div className="row">
+            <RowCard image="https://media-exp1.licdn.com/dms/image/C4D03AQHeZuu5PGQzYw/profile-displayphoto-shrink_200_200/0/1600449353015?e=1613606400&v=beta&t=0291O_ntdB3UE7rg-QaWOiFFi1v5mVsOCGW8aGkbs04" name="stephen00"/>
+            <RowCard image="https://media-exp1.licdn.com/dms/image/C4D03AQHeZuu5PGQzYw/profile-displayphoto-shrink_200_200/0/1600449353015?e=1613606400&v=beta&t=0291O_ntdB3UE7rg-QaWOiFFi1v5mVsOCGW8aGkbs04" name="stephen00"/>
+            <RowCard image="https://media-exp1.licdn.com/dms/image/C4D03AQHeZuu5PGQzYw/profile-displayphoto-shrink_200_200/0/1600449353015?e=1613606400&v=beta&t=0291O_ntdB3UE7rg-QaWOiFFi1v5mVsOCGW8aGkbs04" name="stephen00"/>
+            <RowCard image="https://media-exp1.licdn.com/dms/image/C4D03AQHeZuu5PGQzYw/profile-displayphoto-shrink_200_200/0/1600449353015?e=1613606400&v=beta&t=0291O_ntdB3UE7rg-QaWOiFFi1v5mVsOCGW8aGkbs04" name="stephen00"/>
+            <RowCard image="https://media-exp1.licdn.com/dms/image/C4D03AQHeZuu5PGQzYw/profile-displayphoto-shrink_200_200/0/1600449353015?e=1613606400&v=beta&t=0291O_ntdB3UE7rg-QaWOiFFi1v5mVsOCGW8aGkbs04" name="stephen00"/>
+            <RowCard image="https://media-exp1.licdn.com/dms/image/C4D03AQHeZuu5PGQzYw/profile-displayphoto-shrink_200_200/0/1600449353015?e=1613606400&v=beta&t=0291O_ntdB3UE7rg-QaWOiFFi1v5mVsOCGW8aGkbs04" name="stephen00"/>
+            <RowCard image="https://media-exp1.licdn.com/dms/image/C4D03AQHeZuu5PGQzYw/profile-displayphoto-shrink_200_200/0/1600449353015?e=1613606400&v=beta&t=0291O_ntdB3UE7rg-QaWOiFFi1v5mVsOCGW8aGkbs04" name="stephen00"/>
+            <RowCard image="https://media-exp1.licdn.com/dms/image/C4D03AQHeZuu5PGQzYw/profile-displayphoto-shrink_200_200/0/1600449353015?e=1613606400&v=beta&t=0291O_ntdB3UE7rg-QaWOiFFi1v5mVsOCGW8aGkbs04" name="stephen00"/>
+            <RowCard image="https://media-exp1.licdn.com/dms/image/C4D03AQHeZuu5PGQzYw/profile-displayphoto-shrink_200_200/0/1600449353015?e=1613606400&v=beta&t=0291O_ntdB3UE7rg-QaWOiFFi1v5mVsOCGW8aGkbs04" name="stephen00"/>
+        </div>
+    )
+}
+
+export default Row
